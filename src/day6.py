c59c2aa790a885573221f7232de7fa005b819ac5
clara2911 <clara.tump@hotmail.com>
2022-12-06 17:33:00 +0100

    solutions day 6

--- src/day6.py
+++ src/day6.py
@@ -11,20 +11,44 @@ class Day6:
     def main(self):
         for input_path in self.all_input_paths:
             print(f"\nInput from: {input_path}")
-            instruction_list = self.parse_input(input_path)
-            top_crates = self.part1(instruction_list=instruction_list, stacks=3)
-            top_crates_2 = self.part2(instruction_list=instruction_list, stacks=4)
-            print(f"Solution 1: {top_crates}")
-            print(f"Solution 2: {top_crates_2}")
+            datastream = self.parse_input(input_path)
+            n_processed_chars_before_packet_start = self.part1(datastream=datastream)
+            n_processed_chars_before_message = self.part2(datastream=datastream)
+            print(f"Solution 1: {n_processed_chars_before_packet_start}")
+            print(f"Solution 2: {n_processed_chars_before_message}")
 
     def parse_input(self, input_path):
-        return 4
+        datastream_lines = read_lines_into_list(input_path)
+        assert len(datastream_lines) == 1  # only 1 line
+        return [*datastream_lines[0]]
 
-    def part1(self, instruction_list, stacks):
-        return 5
+    def part1(self, datastream: list):
+        char1, char2, char3 = datastream[0], datastream[1], datastream[2]
+        # print(f"{char1}, {char2}, {char3}")
+        for i, next_char in enumerate(datastream[3:]):
+            # print(i)
+            # print(f"{char1}, {char2}, {char3}, {next_char}")
+            if len(set([char1, char2, char3, next_char])) == 4:
+                num_processed_chars = i + 4
+                return num_processed_chars
+            else:
+                char1 = char2
+                char2 = char3
+                char3 = next_char
+        return 9999999  # something went wrong
 
-    def part2(self, instruction_list, stacks):
-        return 6
+    def part2(self, datastream: list):
+        prev_chars = [datastream[i] for i in range(13)]
+        # print(f"{char1}, {char2}, {char3}")
+        for i, next_char in enumerate(datastream[13:]):
+            # print(i)
+            # print(f"{char1}, {char2}, {char3}, {next_char}")
+            if len(set(prev_chars + [next_char])) == 14:
+                num_processed_chars = i + 14
+                return num_processed_chars
+            else:
+                prev_chars = prev_chars[1:] + [next_char]
+        return 9999999  # something went wrong
 
 
 if __name__ == '__main__':
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -7,6 +7,6 @@ class TestDay6:
         self.input_path = '../inputs/day6/input_test.txt'
 
     def test_that_test_input_returns_what_is_given_in_description_part_1(self):
-        x = self.day6_obj.parse_input(self.input_path)
-        y = self.day6_obj.part1(x, x)
-        assert y
+        datastream = self.day6_obj.parse_input(self.input_path)
+        n_processed_chars = self.day6_obj.part1(datastream)
+        assert n_processed_chars == 11
