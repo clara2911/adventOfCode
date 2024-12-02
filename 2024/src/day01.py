42bbff27ee578e91a8fb65d31b99a2890fa19dbb
ClaraTump <clara.tump@snappet.org>
2024-12-02 08:30:30 +0100

    cleanup day2

---
+++ 2024/src/day01.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import pandas as pd
+
+
+def get_solution_part1(input_path: Path) -> int:
+	coordinates = read_input(input_path)
+	return distance_sum(coordinates)
+
+
+def get_solution_part2(input_path: Path) -> int:
+	coordinates = read_input(input_path)
+	return similarity_score(coordinates)
+
+
+def read_input(input_path: Path) -> pd.DataFrame:
+	coordinates = pd.read_csv(
+		input_path,
+		sep="   ",
+		header=None,
+		names=["search_group1", "search_group2"],
+		dtype={"search_group1": int, "search_group2": int}
+	)
+	return coordinates
+
+
+def similarity_score(coordinates: pd.DataFrame) -> int:
+	value_counts = coordinates['search_group2'].value_counts().to_dict()
+	n_occurrences = [value_counts.get(coord, 0) for coord in coordinates['search_group1']]
+	similarities = coordinates['search_group1'] * n_occurrences
+	return similarities.sum()
+
+
+def distance_sum(coordinates: pd.DataFrame) -> int:
+	coordinates['search_group1'] = coordinates['search_group1'].sort_values().reset_index(drop=True)
+	coordinates['search_group2'] = coordinates['search_group2'].sort_values().reset_index(drop=True)
+	coordinates['difference'] = abs(coordinates['search_group2'] - coordinates['search_group1'])
+	return coordinates['difference'].sum()
--- 2024/test/test_day01.py
+++ 2024/test/test_day01.py
@@ -1,7 +1,7 @@
 import unittest
 from pathlib import Path
 import logging
-import day02
+import day01
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -11,19 +11,19 @@ logger = logging.getLogger(__name__)
 class TestDay01(unittest.TestCase):
 
 	def test_example_distance_sum_matches_puzzle_description(self):
-		distance_sum = day02.get_solution_part1(input_path=Path("../input/day01/test_input_part1.txt"))
+		distance_sum = day01.get_solution_part1(input_path=Path("../input/day01/test_input_part1.txt"))
 		self.assertEqual(distance_sum, 11)
 
 	def test_smoke_test_solution_part1(self):
-		distance_sum = day02.get_solution_part1(input_path=Path("../input/day01/input_day01.txt"))
+		distance_sum = day01.get_solution_part1(input_path=Path("../input/day01/input_day01.txt"))
 		print(f"SOLUTION PART 1 // Distance sum: {distance_sum}")
 
 	def test_example_similarity_score_matches_puzzle_description(self):
-		similarity_score = day02.get_solution_part2(input_path=Path("../input/day01/test_input_part2.txt"))
+		similarity_score = day01.get_solution_part2(input_path=Path("../input/day01/test_input_part2.txt"))
 		self.assertEqual(similarity_score, 31)
 
 	def test_smoke_test_solution_part2(self):
-		similarity_score = day02.get_solution_part2(input_path=Path("../input/day01/input_day01.txt"))
+		similarity_score = day01.get_solution_part2(input_path=Path("../input/day01/input_day01.txt"))
 		print(f"SOLUTION PART 2 // similarity score: {similarity_score}")
 
 
